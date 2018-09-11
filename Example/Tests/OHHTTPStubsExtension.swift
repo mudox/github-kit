433b7aabd8397b45eb6568b788e0b7aca5b74b61
mudox <imudox@gmail.com>
2018-09-11 21:22:11 +0800

    Simplify `OHHTTPStubsResponse` extension initialzers

--- Example/Tests/OHHTTPStubsExtension.swift
+++ Example/Tests/OHHTTPStubsExtension.swift
@@ -94,20 +94,27 @@ fileprivate func parse(messageText text: String)
 
 extension OHHTTPStubsResponse {
 
-  public convenience init(responseMessage text: String) {
-    let (code, header, body) = parse(messageText: text)
+  public convenience init(string: String) {
+    let (code, header, body) = parse(messageText: string)
     self.init(data: body, statusCode: Int32(code), headers: header)
   }
 
-  public convenience init(responseFileURL url: URL) {
-    let data = FileManager.default.contents(atPath: url.absoluteString)!
-    let text = String(data: data, encoding: .utf8)!
-    self.init(responseMessage: text)
+  public convenience init(url: URL) {
+    let text = try! String(contentsOf: url)
+    self.init(string: text)
   }
 
-  public convenience init(responseFileName filename: String, inBundleForClass type: AnyClass) {
-    let path = OHPathForFile(filename, type)!
-    let url = URL(string: path)!
-    self.init(responseFileURL: url)
+  /// Extract status code, header dictionary and body data of a HTTP
+  /// response text stored in the given file.
+  ///
+  /// - Parameters:
+  ///   - filename: The recorded HTTP response text file.
+  ///   - bundle: Bundle from which the file resides, default to main bundle.
+  public convenience init(filename: String, bundle: Bundle = Bundle.main) {
+    let url = bundle.url(
+      forResource: (filename as NSString).deletingPathExtension,
+      withExtension: (filename as NSString).pathExtension
+    )!
+    self.init(url: url)
   }
 }
