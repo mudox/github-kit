66a135f34e3860508339719ac59bfce9b04b923b
mudox <imudox@gmail.com>
2018-09-13 23:09:37 +0800

    Add custom date decoding to `Repository`

--- GitHubKit/Source/Model/Repository.swift
+++ GitHubKit/Source/Model/Repository.swift
@@ -10,16 +10,17 @@ public struct Repository: Decodable {
   public let description: String?
   public let isFork: Bool
 
-  public let creationDate: String
-  public let updateDate: String
-  public let pushDate: String
+  public let creationDate: Date
+  public let updateDate: Date
+  public let pushDate: Date
 
-  public var homepage: String?
+  public let homepage: String?
 
   // Counts
   public let stargazersCount: Int
   public let watchersCount: Int
-  public let subscribersCount: Int?
+  public let openIssuesCount: Int
+  public let forksCount: Int
 
   public let size: Int
 
@@ -29,13 +30,10 @@ public struct Repository: Decodable {
   public let hasDownloads: Bool
   public let hasWiki: Bool
   public let hasPages: Bool
-  public let forksCount: Int
   public let archived: Bool
-  public let openIssuesCount: Int
   public let license: License?
   public let forks: Int
   public let openIssues: Int
-  public let watcherCount: Int
   public let defaultBranch: String
   public let permissions: Permissions?
   public let score: Double
@@ -69,21 +67,88 @@ public struct Repository: Decodable {
     case score
     case size
     case stargazersCount = "stargazers_count"
-    case subscribersCount = "subscribers_count"
     case updateDate = "updated_at"
-    case watcherCount = "watchers"
-    case watchersCount = "watchers_count"
+    case watchersCount = "watchers"
   }
+
+  // swiftlint:disable:next function_body_length
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    archived = try container.decode(Bool.self, forKey: .archived)
+    defaultBranch = try container.decode(String.self, forKey: .defaultBranch)
+    description = try container.decodeIfPresent(String.self, forKey: .description)
+    forks = try container.decode(Int.self, forKey: .forks)
+    forksCount = try container.decode(Int.self, forKey: .forksCount)
+    fullName = try container.decode(String.self, forKey: .fullName)
+    hasDownloads = try container.decode(Bool.self, forKey: .hasDownloads)
+    hasIssues = try container.decode(Bool.self, forKey: .hasIssues)
+    hasPages = try container.decode(Bool.self, forKey: .hasPages)
+    hasProjects = try container.decode(Bool.self, forKey: .hasProjects)
+    hasWiki = try container.decode(Bool.self, forKey: .hasWiki)
+    homepage = try container.decodeIfPresent(String.self, forKey: .homepage)
+    id = try container.decode(Int.self, forKey: .id)
+    isFork = try container.decode(Bool.self, forKey: .isFork)
+    language = try container.decodeIfPresent(String.self, forKey: .language)
+    license = try container.decodeIfPresent(License.self, forKey: .license)
+    name = try container.decode(String.self, forKey: .name)
+    nodeID = try container.decode(String.self, forKey: .nodeID)
+    openIssues = try container.decode(Int.self, forKey: .openIssues)
+    openIssuesCount = try container.decode(Int.self, forKey: .openIssuesCount)
+    owner = try container.decode(Owner.self, forKey: .owner)
+    permissions = try container.decodeIfPresent(Permissions.self, forKey: .permissions)
+    purplePrivate = try container.decode(Bool.self, forKey: .purplePrivate)
+    score = try container.decode(Double.self, forKey: .score)
+    size = try container.decode(Int.self, forKey: .size)
+    stargazersCount = try container.decode(Int.self, forKey: .stargazersCount)
+    watchersCount = try container.decode(Int.self, forKey: .watchersCount)
+
+    // Custom date decoding as RFC3339 format
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+
+    let creationDateString = try container.decode(String.self, forKey: .creationDate)
+    guard let creationDate = formatter.date(from: creationDateString) else {
+      throw DecodingError.dataCorruptedError(
+        forKey: .creationDate, in: container,
+        debugDescription: "parsing `.creationDate` as RFC3339 date failed"
+      )
+    }
+    self.creationDate = creationDate
+
+    let updateDateString = try container.decode(String.self, forKey: .updateDate)
+    guard let updateDate = formatter.date(from: updateDateString) else {
+      throw DecodingError.dataCorruptedError(
+        forKey: .updateDate, in: container,
+        debugDescription: "parsing `.updateDate` as RFC3339 date failed"
+      )
+    }
+    self.updateDate = updateDate
+
+    let pushDateString = try container.decode(String.self, forKey: .pushDate)
+    guard let pushDate = formatter.date(from: pushDateString) else {
+      throw DecodingError.dataCorruptedError(
+        forKey: .pushDate, in: container,
+        debugDescription: "parsing `.pushDate` as RFC3339 date failed"
+      )
+    }
+    self.pushDate = pushDate
+  }
+
+}
+
+public extension Repository {
 
   // MARK: - Repository.License
 
-  // swiftlint:disable nesting
-
-  public struct License: Decodable {
+  struct License: Decodable {
     let key: String
     let name: String
     let spdxID: String?
 
+    // swiftlint:disable:next nesting
     private enum CodingKeys: String, CodingKey {
       case key
       case name
@@ -93,13 +158,14 @@ public struct Repository: Decodable {
 
   // MARK: - Repository.Owner
 
-  public struct Owner: Decodable {
+  struct Owner: Decodable {
     public let login: String
     public let id: Int
     public let avatarURL: URL
     public let type: String
     public let siteAdmin: Bool
 
+    // swiftlint:disable:next nesting
     private enum CodingKeys: String, CodingKey {
       case avatarURL = "avatar_url"
       case id
@@ -111,7 +177,7 @@ public struct Repository: Decodable {
 
   // MARK: - Repository.Permission
 
-  public struct Permissions: Codable {
+  struct Permissions: Codable {
     public let admin: Bool
     public let push: Bool
     public let pull: Bool
