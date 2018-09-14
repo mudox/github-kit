f7de21f3a39f7a600444087065f9e855ef410822
mudox <imudox@gmail.com>
2018-09-14 12:06:28 +0800

    Fix `Tree` coding keys

--- GitHubKit/Source/Model/Git Object/Tree.swift
+++ GitHubKit/Source/Model/Git Object/Tree.swift
@@ -4,6 +4,13 @@ public struct Tree: Decodable {
   public let sha: String
   public let truncated: Bool
   public let members: [Member]
+
+  private enum CodingKeys: String, CodingKey {
+    case sha
+    case truncated
+    case members = "tree"
+  }
+
 }
 
 public extension Tree {
