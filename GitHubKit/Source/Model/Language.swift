b1186ff3a7ccd4711a28b29c9e3df38fa61ae16e
mudox <imudox@gmail.com>
2018-09-18 22:27:16 +0800

    Add `Language.swift`

---
+++ GitHubKit/Source/Model/Language.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public struct Language {
+
+  private static let listURL = URL(string: "https://raw.githubusercontent.com/ozh/github-colors/master/colors.json")
+
+  static let all: [Language] {
+    fatalError("Unimplemented")
+  }
+  
+}
