27fe8839ea98fa862a1f30c28d36a1a0ae0cf063
mudox <imudox@gmail.com>
2018-10-08 18:40:21 +0800

    Update `Auth` in test bundle

--- Example/GitHubKit_Tests/GitHubServiceSpecs/Auth.swift
+++ Example/GitHubKit_Tests/GitHubServiceSpecs/Auth.swift
@@ -2,19 +2,19 @@ import Foundation
 
 struct Auth {
 
-  static var accessToken: String? {
+  static var token: String? {
     get { return UserDefaults.standard.string(forKey: "accessToken") }
     set { UserDefaults.standard.set(newValue, forKey: "accessToken") }
   }
 
-  enum User {
-    static let name = "cement_ce@163.com"
-    static let password = "zheshi1geceshihao"
-  }
+  static let user = (
+    name: "cement_ce@163.com",
+    password: "zheshi1geceshihao"
+  )
 
-  enum App {
-    static let key = "c2dc8eb1cd09ac1e2381"
-    static let secret = "1e6558ef27cd7844047b9de69dc009d0bd05579b"
-  }
-  
+  static let app = (
+    key: "c2dc8eb1cd09ac1e2381",
+    secret: "1e6558ef27cd7844047b9de69dc009d0bd05579b"
+  )
+
 }
