263637ec90fc475502c0a0c9b2b4ba2f19f1c266
mudox <imudox@gmail.com>
2018-10-09 23:43:37 +0800

    Refactor `AuthPlugin` to use `CredentialProvider`

--- GitHubKit/Service/Plugins/AuthPlugin.swift
+++ GitHubKit/Service/Plugins/AuthPlugin.swift
@@ -2,27 +2,27 @@ import Moya
 
 import JacKit
 
+private let jack = Jack("GitHub.AuthPlugin").set(options: .short)
+
+public protocol CredentialProdiver {
+  var token: String? { get }
+  var user: (name: String, password: String)? { get }
+  var app: (key: String, secret: String)? { get }
+}
+
 public class AuthPlugin: PluginType {
 
-  public var token: String?
-  public var user: (name: String, password: String)?
-  public var app: (key: String, secret: String)?
+  private let credentialProvider: CredentialProdiver
 
-  public init(
-    token: String? = nil,
-    user: (name: String, password: String)? = nil,
-    app: (key: String, secret: String)? = nil
-  ) {
-    self.token = token
-    self.user = user
-    self.app = app
+  public init(credentialProvider: CredentialProdiver) {
+    self.credentialProvider = credentialProvider
   }
 
-  public func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
-    let jack = Jack("GitHubKit.AuthPlugin").set(options: .short)
+  // MARK: - PluginType
 
+  public func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
     guard let target = target as? APIv3 else {
-      jack.warn("the target type is not `GitHub.APIv3`")
+      jack.descendant("prepare").warn("the target type is not `GitHub.APIv3`")
       return request
     }
 
@@ -32,21 +32,21 @@ public class AuthPlugin: PluginType {
     case .none:
       break
     case .user:
-      guard let (name, password) = user else {
+      guard let (name, password) = credentialProvider.user else {
         jack.warn("username & password is missing")
         return request
       }
       let field = Headers.Authorization.user(name: name, password: password)
       request.setValue(field, forHTTPHeaderField: "Authorization")
     case .app:
-      guard let (key, secret) = app else {
+      guard let (key, secret) = credentialProvider.app else {
         jack.warn("app key & secret is missing")
         return request
       }
       let field = Headers.Authorization.app(key: key, secret: secret)
       request.setValue(field, forHTTPHeaderField: "Authorization")
     case .token:
-      guard let token = token else {
+      guard let token = credentialProvider.token else {
         jack.warn("access token is missing")
         return request
       }
