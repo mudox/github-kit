b15cf95c4d8d04e3aacb7a8fd019b9e727f44db8
mudox <imudox@gmail.com>
2018-10-20 23:18:27 +0800

    Rename to `HTTPStubbing`

--- Demo/Tests/HTTPStubbing/HTTPStubbing.swift
+++ Demo/Tests/HTTPStubbing/HTTPStubbing.swift
@@ -4,14 +4,13 @@ import OHHTTPStubs
 
 import JacKit
 
-public struct NetworkStubbing {
+public struct HTTPStubbing {
 
   private init() {}
 
   /// Set `$HTTP_STUBBING` to 'YES' to enabled stubbing.
   public static var isEnabled: Bool = {
-    let environValue = ProcessInfo.processInfo.environment["HTTP_STUBBING"] ?? "NO"
-    let enabled = (environValue == "YES")
+    let enabled = ProcessInfo.processInfo.environment["HTTP_STUBBING"] == "enable"
     let jack = Jack("NetworkStubbing")
 
     if enabled {
