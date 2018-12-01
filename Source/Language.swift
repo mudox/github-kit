44f3f2871f7c90b81c80b040b8e96378a3345fc0
mudox <imudox@gmail.com>
2018-12-01 22:47:08 +0800

    Move `GitHub.Language` decoding onto background thread

--- Source/Language.swift
+++ Source/Language.swift
@@ -19,11 +19,13 @@ extension Language {
   public static var all: Single<[Language]> {
     let url = URL(string: "https://github.com/github/linguist/raw/master/lib/linguist/languages.yml")!
     return RxAlamofire.string(.get, url)
+      .observeOn(ConcurrentDispatchQueueScheduler(qos: .userInitiated))
       .map { string -> [Language] in
         let decoder = YAMLDecoder()
-        return try decoder.decode([String: YAML].self, from: string).map { key, value in
-          Language(name: key, color: value.color)
-        }
+        return try decoder.decode([String: YAML].self, from: string)
+          .map { key, value in
+            Language(name: key, color: value.color)
+          }
       }
       .asSingle()
   }
