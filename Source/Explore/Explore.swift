27e9a1974a557bebc4c64bac9cfb1f7f3c315c0c
mudox <imudox@gmail.com>
2018-12-16 00:27:15 +0800

    Remove caching logic from `Explore`

--- Source/Explore/Explore.swift
+++ Source/Explore/Explore.swift
@@ -9,56 +9,54 @@ import Yams
 
 import JacKit
 
-public struct Explore {
+private let jack = Jack("GitHub.Explore").set(format: .short)
 
-  private static let downloadURL = URL(string: "https://github.com/github/explore/archive/master.zip")!
+private extension URL {
 
-  /// Application Support/GitHubKit/GitHub.Explore
-  private static let rootDirectoryURL: URL = {
+  static let download = URL(string: "https://github.com/github/explore/archive/master.zip")!
+
+  /// Application Support/GitHubKit/GitHub.Explore/
+  static let prefix: URL = {
     let url = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
     return url.appendingPathComponent("GitHubKit/GitHub.Explore")
   }()
 
-  /// Application Support/GitHubKit/GitHub.Explore/master.zip
-  private static let downloadedZipFileURL = rootDirectoryURL.appendingPathComponent("downloaded.zip")
+  /// Application Support/GitHubKit/GitHub.Explore/downloaded.zip
+  static let downloaded = prefix.appendingPathComponent("downloaded.zip")
 
-  /// Application Support/GitHubKit/GitHub.Explore/unzipped
-  internal static let unzippedDirectoryURL = rootDirectoryURL.appendingPathComponent("explore-master")
+  /// Application Support/GitHubKit/GitHub.Explore/explore-master/
+  static let unzipped = prefix.appendingPathComponent("explore-master")
 
-  private static var isCached: Bool {
-    let jack = Jack("GitHub.Explore.isCached").set(format: .short)
-    if  FileManager.default.fileExists(atPath: unzippedDirectoryURL.path) {
-      jack.verbose("repo github/explore is cached")
-      return true
-    } else {
-      jack.verbose("repo github/explore is NOT cached")
-      return false
-    }
-  }
+  /// Application Support/GitHubKit/GitHub.Explore/explore-master/topics/
+  static let topics = unzipped.appendingPathComponent("topics")
+  /// Application Support/GitHubKit/GitHub.Explore/explore-master/collections/
+  static let collections = unzipped.appendingPathComponent("collections")
 
-  // MARK: - Interface
+}
+
+// MARK: - GitHub.Explore
+
+public enum Explore {
 
   public enum Error: Swift.Error {
     case regexMatchingMarkdownContent
   }
 
-  public static var synchronize: Completable {
+  private static var download: Completable {
 
     return .create { completable in
-      Jack("GitHub.Explore.synchronize").info("download github/explore/master.zip ...")
-
-      let request = URLRequest(url: downloadURL)
+      let request = URLRequest(url: .download)
 
       let destination: DownloadRequest.DownloadFileDestination = { _, _ in
-        (downloadedZipFileURL, [.removePreviousFile, .createIntermediateDirectories])
+        (.downloaded, [.removePreviousFile, .createIntermediateDirectories])
       }
 
       return RxAlamofire.download(request, to: destination)
         .observeOn(ConcurrentDispatchQueueScheduler(qos: .userInitiated))
         .subscribe(onCompleted: {
           SSZipArchive.unzipFile(
-            atPath: downloadedZipFileURL.path,
-            toDestination: downloadedZipFileURL.deletingLastPathComponent().path
+            atPath: URL.downloaded.path,
+            toDestination: URL.downloaded.deletingLastPathComponent().path
           )
           completable(.completed)
         })
@@ -66,32 +64,55 @@ public struct Explore {
 
   }
 
-  public static var loadCuratedTopics: Single<[CuratedTopic]> {
-    return .create { single in
+  private static var parse: Single<(topics: [CuratedTopic], collections: [Collection])> {
 
+    return .create { single in
       // Parse folder
       do {
-        let topicsDirectoryURL = unzippedDirectoryURL.appendingPathComponent("topics")
 
-        let curatedTopics = try FileManager.default
-          .contentsOfDirectory(atPath: topicsDirectoryURL.path)
-          // Append `/topics/index.md`
+        // Curated topics
+
+        let topics = try FileManager.default
+          // For each directory in `topics` folder
+          .contentsOfDirectory(atPath: URL.topics.path)
+          // Append `index.md`
           .map { path -> URL in
-            let markdownPath = path.appending("/index.md")
-            return unzippedDirectoryURL.appendingPathComponent("/topics/\(markdownPath)")
+            return URL.topics.appendingPathComponent("/\(path)/index.md")
           }
           // Filter out non-existing files.
           .filter { url in
             FileManager.default.fileExists(atPath: url.path)
           }
-          // Parse each `index.md` file as an instance of `GitHub.Explore.CuratedTopic`
+          // Parse each `index.md` file as an instance of `GitHub.CuratedTopic`
           .map { url -> CuratedTopic in
+            let baseDir = url.deletingLastPathComponent()
             let text = try String(contentsOf: url)
             let (yamlString, description) = try parse(text: text)
-            return try CuratedTopic(yamlString: yamlString, description: description)
+            return try CuratedTopic(yamlString: yamlString, description: description, baseDir: baseDir)
           }
 
-        single(.success(curatedTopics))
+        // Collections
+
+        let collections = try FileManager.default
+          // For each directory in `collections` folder
+          .contentsOfDirectory(atPath: URL.collections.path)
+          // Append `index.md`
+          .map { path -> URL in
+            return URL.collections.appendingPathComponent("/\(path)/index.md")
+          }
+          // Filter out non-existing files.
+          .filter { url in
+            FileManager.default.fileExists(atPath: url.path)
+          }
+          // Parse each `index.md` file as an instance of `GitHub.Explore.Collection`
+          .map { url -> Collection in
+            let baseDir = url.deletingLastPathComponent()
+            let text = try String(contentsOf: url)
+            let (yamlString, description) = try parse(text: text)
+            return try Collection(yamlString: yamlString, description: description, baseDir: baseDir)
+          }
+
+        single(.success((topics: topics, collections: collections)))
       } catch {
         single(.error(error))
       }
@@ -100,64 +121,8 @@ public struct Explore {
     } // return .create
   }
 
-  /// Scan the downloaded 'github/explore' folder, parse each 'topics/\*/index.md'
-  /// file as an instance of `GitHub.Explore.CuratedTopic`.
-  public static func curatedTopics(afterSync sync: Bool = false) -> Single<[CuratedTopic]> {
-    if !isCached || sync {
-      return synchronize.andThen(loadCuratedTopics)
-    } else {
-      return loadCuratedTopics
-    }
-  }
-
-  public static var loadCollections: Single<[Collection]> {
-    return .create { single in
-
-      // Unzip
-      SSZipArchive.unzipFile(
-        atPath: downloadedZipFileURL.path,
-        toDestination: downloadedZipFileURL.deletingLastPathComponent().path
-      )
-
-      // Parse folder
-      do {
-        let collectionsDirectoryURL = unzippedDirectoryURL.appendingPathComponent("collections")
-
-        let collections = try FileManager.default
-          .contentsOfDirectory(atPath: collectionsDirectoryURL.path)
-          // Append `/collections/index.md`
-          .map { path -> URL in
-            let markdownPath = path.appending("/index.md")
-            return unzippedDirectoryURL.appendingPathComponent("/collections/\(markdownPath)")
-          }
-          // Filter out non-existing files.
-          .filter { url in
-            FileManager.default.fileExists(atPath: url.path)
-          }
-          // Parse each `index.md` file as an instance of `GitHub.Explore.Collection`
-          .map { url -> Collection in
-            let text = try String(contentsOf: url)
-            let (yamlString, description) = try parse(text: text)
-            return try Collection(yamlString: yamlString, description: description)
-          }
-
-        single(.success(collections))
-      } catch {
-        single(.error(error))
-      }
-
-      return Disposables.create()
-    }
-  }
-
-  /// Scan the downloaded 'github/explore' folder, parse each 'collections/\*/index.md'
-  /// file as an instance of `GitHub.Explore.Collection`.
-  public static func collections(afterSync sync: Bool = false) -> Single<[Collection]> {
-    if !isCached || sync {
-      return synchronize.andThen(loadCollections)
-    } else {
-      return loadCollections
-    }
+  public static var topicsAndCollections: Single<(topics: [CuratedTopic], collections: [Collection])> {
+    return download.andThen(parse)
   }
 
 }
