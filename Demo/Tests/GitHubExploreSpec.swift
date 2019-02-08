00ef1acc2381fd089bb4a8e956465c8d779f89ac
mudox <imudox@gmail.com>
2019-02-08 20:21:32 +0800

    Remove `RxAlamofire`

--- Demo/Tests/GitHubExploreSpec.swift
+++ Demo/Tests/GitHubExploreSpec.swift
@@ -14,20 +14,25 @@ class GitHubExploreSpec: QuickSpec { override func spec() {
   beforeEach(Fixtures.setup)
   afterEach(Fixtures.cleanup)
 
-  it("loads topic and collection lists") {
+  fit("loads topic and collection lists") {
     // Arrange
-    let jack = Jack("Test.GitHub.Explore.lists")
+    let jack = Jack("Test.GitHub.Explore.lists").set(format: .short)
 
     // Act, Assert
     waitUntil { done in
       _ = GitHub.Explore.lists
         .subscribe(
-          onSuccess: { lists in
-            jack.sub("onSuccess").info("""
-            - Topics count: \(lists.topics.count)
-            - Collections count: \(lists.collections.count)
-            """)
-            done()
+          onNext: { state in
+            switch state {
+            case let .success(lists):
+              jack.sub("onSuccess").info("""
+              - Topics count: \(lists.topics.count)
+              - Collections count: \(lists.collections.count)
+              """)
+              done()
+            default:
+              jack.func().debug("\(state)")
+            }
           },
           onError: { error in
             jack.sub("onError").error(dump(of: error))
